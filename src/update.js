d96c427c9586c466ec4932cf53e1c00c14fa53ff
stefan22 <stefan.emailacct@gmail.com>
2020-12-06 20:27:03 +0000

    [showForm] update

--- src/update.js
+++ src/update.js
@@ -1,4 +1,24 @@
+const MSGS = {
+  SHOW_FORM: 'SHOW_FORM',
+};
+
+export function showFormMsg(showForm) {
+  return {
+    type: MSGS.SHOW_FORM,
+    showForm,
+  };
+}
+
 function update(msg, model) {
+  console.log(model);
+  switch (msg.type) {
+    case MSGS.SHOW_FORM:
+      return {
+        ...model,
+        showForm: msg.showForm,
+      };
+  }
+  console.log(model);
   return model;
 }
 
--- src/view.js
+++ src/view.js
@@ -1,33 +1,72 @@
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
-import model from './model';
+//import model from './model';
+import { showFormMsg } from './update';
 import './styles/calcounter.css';
 
 const { pre, div, h1, button, label, input, form } = hh(h);
-const { description, calories } = model;
 
-function fieldset(lb, inpt) {
+function fieldset(labelText, inputValue) {
   return div({ className: 'cal-field' }, [
-    label({ className: 'cal-label' }, lb),
+    label({ className: 'cal-label' }, labelText),
     input({
       className: 'cal-input',
       type: 'text',
-      value: inpt,
+      value: inputValue,
     }),
   ]);
 }
 
-function formView(dispatch, model) {
-  return form({ className: 'cal-form' }, [
-    fieldset('Meal', description),
-    fieldset('Calories', calories),
-    button({ className: 'cal-btn' }, 'Add Meal'),
+function buttonSet(dispatch) {
+  return div({ className: 'cal-btn-wrap' }, [
+    button(
+      {
+        className: 'cal-btn',
+        type: 'submit',
+      },
+      'Save',
+    ),
+    button(
+      {
+        className: 'cal-btn',
+        type: 'button',
+        onclick: () => dispatch(showFormMsg(false)),
+      },
+      'Cancel',
+    ),
   ]);
+}
+
+function formView(dispatch, model) {
+  const { description, calories, showForm } = model;
+  if (showForm) {
+    return form(
+      {
+        className: 'cal-form',
+        onsubmit: e => {
+          e.preventDefault();
+        },
+      },
+      [
+        fieldset('Meal', description),
+        fieldset('Calories', calories || ''),
+        buttonSet(dispatch),
+      ],
+    );
+  }
+
+  return button(
+    {
+      className: 'cal-btn',
+      onclick: () => dispatch(showFormMsg(true)),
+    },
+    'Add Meal',
+  );
 }
 
 function view(dispatch, model) {
   return div({ className: 'cal-wrapper' }, [
-    h1({ className: 'cal-heading' }, 'Calories'),
+    h1({ className: 'cal-heading' }, 'Calorie Counter'),
     formView(dispatch, model),
     pre(JSON.stringify(model, null, 2)),
   ]);
